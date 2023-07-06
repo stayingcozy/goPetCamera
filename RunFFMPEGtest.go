ebd8afa326aa8050d436c4b310244ca9163ed037
stayingcozy <cody.colclough@gmail.com>
2023-07-06 11:20:39 -0400

    first try at one one instance of ffmpeg go routine

--- RunFFMPEGtest.go
+++ RunFFMPEGtest.go
@@ -7,7 +7,12 @@ import (
 	"os/exec"
 )
 
-func RunFFMPEGtest() {
+func RunFFMPEGtest(ffmpegLock chan struct{}) {
+
+	// Release the lock when the function exits
+	defer func() { <-ffmpegLock }() 
+
+
 	// Set up the command and arguments
 	cmd := exec.Command("ffmpeg", "-re", "-f", "lavfi", "-i", "testsrc=size=640x480:rate=30", "-vcodec", "libvpx", "-cpu-used", "5", "-deadline", "1", "-g", "10", "-error-resilient", "1", "-auto-alt-ref", "1", "-f", "rtp", "rtp://127.0.0.1:5004?pkt_size=1200")
 
