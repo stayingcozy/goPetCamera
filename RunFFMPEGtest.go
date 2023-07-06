9ad9d6a7d5362d2295c6ab095bf2d839d0d8efc9
stayingcozy <cody.colclough@gmail.com>
2023-07-06 13:40:04 -0400

    false alarm on ffmpeg. back to baseline

--- RunFFMPEGtest.go
+++ RunFFMPEGtest.go
@@ -7,12 +7,8 @@ import (
 	"os/exec"
 )
 
-func RunFFMPEGtest(ffmpegLock chan struct{}) {
-
-	// Release the lock when the function exits
-	defer func() { <-ffmpegLock }() 
-
-
+func RunFFMPEGtest() {
+	
 	// Set up the command and arguments
 	cmd := exec.Command("ffmpeg", "-re", "-f", "lavfi", "-i", "testsrc=size=640x480:rate=30", "-vcodec", "libvpx", "-cpu-used", "5", "-deadline", "1", "-g", "10", "-error-resilient", "1", "-auto-alt-ref", "1", "-f", "rtp", "rtp://127.0.0.1:5004?pkt_size=1200")
 
